cf87c3327cea32072cc6cdc4bb3c5b846423345b
bsk-123 <43913935+bsk-123@users.noreply.github.com>
2020-07-20 13:59:07 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+
+document.querySelector(".user input").addEventListener("keydown",function(event){
+    
+   var t = event.key
+   var v = document.querySelector(".user input").value
+    if(t=="Backspace"){
+        if(v.length==1){
+            document.querySelector(".ui").style.display="inline-block"
+            document.querySelector(".uii").style.display="inline-block"
+            document.querySelector(".uiii").style.display="inline-block"
+            document.querySelector(".sd").style.display="none"
+        }
+    }else{
+        if(v==""){
+            document.querySelector(".ui").style.display="none"
+            document.querySelector(".uii").style.display="none"
+            document.querySelector(".uiii").style.display="none"
+            document.querySelector(".sd").style.display="inline-block"
+        }
+    }
+})
